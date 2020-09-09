b337fb8fccd1fff935f9a1ae62b6e2150477f2ae
danya <ijluxa2001@gmail.com>
2020-09-09 14:27:16 +0300

    first task complited

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,1 +1,14 @@
 var color = prompt("Введите цвет", "")
+if (color === "red"){
+    document.write("<div style='background-color: red;'>красный</div>");
+    document.write("<div style='background-color: black; color: white;'>черный</div>");
+}else if(color === "black"){
+    document.write("<div style='background-color: black; color: white;'>черный</div>");
+}else if(color === "blue"){
+    document.write("<div style='background-color: blue;'>синий</div>");
+    document.write("<div style='background-color: green;'>зеленый</div>");
+}else if(color === "green"){
+    document.write("<div style='background-color: green;'>зеленый</div>");
+}else{
+    document.write("<div style='background-color: gray;'>Я не понял</div>");
+}
