46a045248c01520bcb1c8c0b9960a9dbd6a04983
Danylo Iliukha <ijluxa2001@gmail.com>
2020-09-15 10:06:42 +0300

    done

--- HW-2/script.js
+++ HW-2/script.js
@@ -59,3 +59,111 @@ if (personAge > 0 && personAge < 10){
 } else {
     alert("Человек ли ты ?" + greeting)
 }
+var JSON = {
+    tagName: "body",
+    subTags: {
+        div: {
+            tagName: "div",
+            subTags: {
+                span: {
+                    tagName: "span",
+                    text: "Enter a data please:"
+                },
+                br: {
+                    tagName: "br"
+                },
+                input: {
+                    tagName: "input",
+                    attrs: {
+                        type: "text",
+                        id: "name"
+                    }
+                },
+                input: {
+                    tagName: "input",
+                    attrs: {
+                        type: "text",
+                        id: "surname"
+                    }
+                }
+            }
+        },
+        div: {
+            tagName: "div",
+            subTags: {
+                button: {
+                    tagName: "button",
+                    attrs: {
+                        id: "ok"
+                    },
+                    text: "OK"
+                },
+                button: {
+                    tagName: "button",
+                    attrs: {
+                        id: "cancel"
+                    },
+                    text: "Cancel"
+                }
+            }
+        }
+    }
+}
+var notebook = {
+    brand: prompt("Input notebook brand", ""),
+    type: prompt("What type is it?", ""),
+    model: prompt("What model is it?", ""),
+    ram: prompt("How much RAM it has?", ""),
+    size: prompt("Whats the size of it?", ""),
+    weight: prompt("Whats the weight of it?", ""),
+    resolution: {
+        width: prompt("Input horizontal resolution", ""),
+        height: prompt("Input vertical resolution", ""),
+    },
+};
+
+var phone = {
+    brand: prompt("Input phone brand", ""),
+    model: prompt("Whats model is it?", ""),
+    ram: prompt("How much RAM it has?", ""),
+    color: prompt("What color is the case?", ""),
+};
+
+var person = {
+    name: prompt("Input your name", ""),
+    surname: prompt("Input your surname", ""),
+    married: confirm("Are you married?"),
+}
+
+while (!confirm()){}
+
+var personNumber = prompt("Загадайте число")
+personNumber = +personNumber
+for (let i = 0; i < personNumber; i++){
+    console.log(i)
+}
+
+var random
+var counter = 0
+while(true){
+    random = Math.random()
+    counter++
+    if (random > 0.9){
+        break
+    }
+}
+alert(counter)
+
+var length = prompt('Введите длину строки')
+var outstr = ''
+for (let i = 0; i < +length; i++){
+    outstr += '#'
+}
+alert(outstr)
+
+var array  = []
+let size = prompt('input size of array')
+for (let i = 0; i < +size; i++){
+    array.push(Math.pow(i,3))
+}
+console.log(array)
